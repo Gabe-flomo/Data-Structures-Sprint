9c2659e601ec53a5aded22f2d720d311b1dfb644
gabeflomo <gabeflomo821@gmail.com>
2020-02-28 13:01:13 -0600

    1:00 push

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -41,7 +41,60 @@ class LinkedList:
       current = current.get_next()
     # if we've gotten here, then the target node isn't in our list
     return False
-
+  
+  # reverse the pointers of each node
+  # [1]->[2]->[3]->None
+  # none<-[1]<-[2]<-[3]
+  #
+  # prev = none
+  # current = head     == [1]
+  # head = head.next  == [2]
+  # [1] needs to point to none since it will be the new tail [none]<-[1] [2]->[3]->[none]
+  # current.next = prev    (just like in a DLL)
+  # prev = current     == [1]
+  #
+  # loop
+  # 
+  # prev = [1]
+  # current = head     == [2]
+  # head = head.next  == [3]
+  # [2] needs to point to [1]
+  # current.next = prev
+  # prev = current
+  #
+  # loop
   def reverse_list(self):
-    # TO BE COMPLETED
-    pass
+    prev = None
+    
+    while self.head:
+      # save the head as the current node
+      current = self.head
+
+      # make the new head the next value
+      self.head = self.head.next_node
+
+      # make the currents next node point to the node before it
+      current.next_node = prev
+
+      # make the previous node the old head
+      prev = current
+
+      
+      
+
+  def printlist(self):
+    content = []
+    head = self.head
+    while head:
+      content.append(head.value)
+      head = head.next_node
+    
+    return content
+
+ll = LinkedList()
+ll.add_to_head(1)
+ll.add_to_head(2)
+ll.add_to_head(3)
+#print(ll.printlist())
+ll.reverse_list()
+print(ll.printlist())
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -14,7 +14,6 @@ class RingBuffer:
             self.current = self.storage.head
         # if the list is full start overwriting from the head
         elif self.storage.length == self.capacity:
-            # while the head isnt none
             if self.current:
                 print("current",self.current.value)
                 # make the new item the new head
@@ -24,11 +23,16 @@ class RingBuffer:
                 print(f"removing {self.storage.head.next.value}")
                 self.storage.head.next.delete()
                 # make the next node the new head
-                print(f"making {self.storage.head.next.value} the new head")
+                #print(f"making {self.storage.head.next.value} the new head")
                 self.current = self.storage.head.next
                 print("current",self.current.value)
-            
+                print()
 
+
+
+            
+            
+            
             
 
 
@@ -40,7 +44,7 @@ class RingBuffer:
         while head is not None:
             list_buffer_contents.append(head.value)
             head = head.next
-        
+            
 
         return list_buffer_contents
 
@@ -57,7 +61,38 @@ class ArrayRingBuffer:
     def get(self):
         pass
 
+'''
+[1][2][3]
+add [4]
+current = head == [1]
+head = head.next = [2]
+head.prev = value
 
+[4][2][3]
+add [5]
+current = head == [2]
+head = head.next == [3]
+current = value
+[4][5][3]
+add [6]
+[4][5][6]
+add [7]
+[7][5][6]
+
+if self.current:
+                print("current",self.current.value)
+                # make the new item the new head
+                print(f"making {item} the new head")
+                self.storage.add_to_head(item)
+                # remove the previous head
+                print(f"removing {self.storage.head.next.value}")
+                self.storage.head.next.delete()
+                # make the next node the new head
+                print(f"making {self.storage.head.next.value} the new head")
+                self.current = self.storage.head.next
+                print("current",self.current.value)
+
+'''
 buf = RingBuffer(3)
 buf.append(1)
 buf.append(2)
@@ -66,5 +101,4 @@ print(buf.get())
 buf.append(4)
 print(buf.get())
 
-buf.append(5)
-print(buf.get())
+
